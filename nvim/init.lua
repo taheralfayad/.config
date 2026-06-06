4f9354fe9851131828340d9ca11cb0555d88c4ca
Taher Alfayad <taheralfayad@gmail.com>
2026-06-05 20:37:08 -0400

    feat(nvim): add mason

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,13 @@
+vim.pack.add{
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/mason-org/mason.nvim' },
+	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+}
+
+require('mason').setup()
+require('mason-lspconfig').setup()
+
 vim.opt["tabstop"]=4
 vim.opt["shiftwidth"]=4
+
+vim.keymap.set('n', '<leader>ev', ':e $MYVIMRC<CR>', { desc = 'Edit init.lua' })
