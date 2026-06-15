a48a0562428f7c37524a47804805da7e257b29a1
Ahmad Altaher Alfayad <taheralfayad@gmail.com>
2026-06-15 11:30:39 -0400

    (feat: nvim): show hidden files with telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,9 @@ vim.opt["tabstop"]=4
 vim.opt["shiftwidth"]=4
 
 vim.keymap.set('n', '<leader>ev', ':e $MYVIMRC<CR>', { desc = 'Edit init.lua' })
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files({ hidden = true, no_ignore = true })
+end, { desc = 'Telescope find files including hidden' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
