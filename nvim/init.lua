e29a0b72f3616007783c3da35b2d823a718227cd
Taher Alfayad <taheralfayad@gmail.com>
2026-07-03 12:28:01 -0400

    merge init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -72,3 +72,22 @@ end, { desc = "Toggle inlay hints" })
 vim.keymap.set("i", "<leader>c", function()
 	vim.lsp.completion.get()
 end, { desc = "Trigger LSP completion" })
+
+-- autoimport missing imports
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.go",
+	callback = function()
+		local params = vim.lsp.util.make_range_params()
+		params.context = { only = { "source.organizeImports" } }
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+		for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+		end
+	end,
+})
